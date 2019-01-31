faaa9fecee8c1b382ba6a8d6993bcf76b55d24f2
ArthurDrc <44248355+ArthurDrc@users.noreply.github.com>
2019-01-31 11:31:13 +0800

    Update enus_version.sh

--- enus_version.sh
+++ enus_version.sh
@@ -1,13 +1,17 @@
-echo "SSH key generating v1.0"
+#!/bin/bash
+echo "----------------"
+echo "----SSH-GEN-----"
+echo "--BY:ArthurDrc--"
+echo "----------------"
 echo "Enter the username your want your ssh key for:"
 read username
-echo "Enter the name you want your SSH keys to be"
+echo "Enter the name you want your SSH keys to be:"
 read name
 sudo ssh-keygen  -C $username -f $name
 sudo ssh-add $name
-echo "Complete, keys saved in the directory where you run this script"
 echo "Content of private key:"
 sudo cat $name
 echo "Content of public key:"
 sudo cat ${name}.pub
-echo "Thanks for using!"
+echo "Complete, keys saved in the directory where you run this script and the identity is added."
+echo “Thanks for using!”
